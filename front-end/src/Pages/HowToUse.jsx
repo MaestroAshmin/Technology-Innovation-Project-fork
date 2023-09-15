349c7f352d26614f155eb2545017882b706636f0
Le Vy Cao <levycao@192-168-1-120.tpgi.com.au>
2023-09-15 12:36:49 +1000

    add instruction page

--- front-end/src/Pages/HowToUse.jsx
+++ front-end/src/Pages/HowToUse.jsx
@@ -1,10 +1,17 @@
 /*How-to-use page
 Le Vy Cao 104201234
-Last edited 14/09/2023*/
+Last edited 15/09/2023*/
 import React from 'react';
-
+import Navbar from '../Components/Navbar';
+import Instructions from '../Components/Instructions';
+import Footer from '../Components/Footer'
 function HowToUse() {
-  return <h2>How to Use Page</h2>;
+
+  return <>
+  <Navbar/>
+  <Instructions/>
+  <Footer/>
+  </>;
 }
 
 export default HowToUse;
