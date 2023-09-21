18453b0dd00966bb8aa8683e53889ca7472d2273
justin <128043211+baronstink@users.noreply.github.com>
2023-09-21 13:14:35 +1000

    Fixes
    Modified to account for fixed navbar, and to include agreed upon risk exposure classification drop down in the log test page in lieu of the reason for testing text box.

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,13 +1,19 @@
 /*login page
 Justin Li 104138316
-Last edited 14/09/2023*/
+Last edited 21/09/2023*/
 import React from 'react';
 import Login from '../Components/LoginComp';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer'
 
 function LoginPage() {
   return (
-    <div className="App">
+    <div className="page">
+      <Navbar />
+      <div className = 'form-container'>
       <Login />
+      </div>
+      <Footer />
     </div>
   );
 }
--- front-end/src/Pages/Register.jsx
+++ front-end/src/Pages/Register.jsx
@@ -1,12 +1,18 @@
 /*register page
 Justin Li 104138316
-Last edited 14/09/2023*/
+Last edited 21/09/2023*/
 import React from 'react';
 import Register from '../Components/RegisterComp';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer'
 function RegisterPage() {
   return (
-    <div className="registerPage">
+    <div className='page'>
+    <Navbar />
+    <div className="form-container">
       <Register />
+    </div>
+    <Footer/>
     </div>
   );
 }
