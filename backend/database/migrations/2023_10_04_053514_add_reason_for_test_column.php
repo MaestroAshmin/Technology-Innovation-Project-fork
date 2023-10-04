95a2c44a5680bde9351cd11c245381de3cb5833b
Bruce Le <brucele.tt168@gmail.com>
2023-10-04 16:39:27 +1100

    add required reason_for_test column

---
+++ backend/database/migrations/2023_10_04_053514_add_reason_for_test_column.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('test_result', function (Blueprint $table) {
+            // add reason_for_test column
+            $table->text('reason_for_test')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('test_result', function (Blueprint $table) {
+            // drop reason_for_test column
+            $table->dropColumn('text_exposure_reason');
+        });
+    }
+};
